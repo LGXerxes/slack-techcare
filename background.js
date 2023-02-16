403946a91b753bd1d493809cc2758db2d1bf1c2c
LGXerxes <ditisluca@yahoo.it>
2023-02-16 15:31:57 +0100

    only when seeing the channel tech-care

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 setInterval(ch, 1200);
 
 function ch() {
-    var l = document.querySelectorAll('[alt=":white_check_mark:"]');
-    l.forEach(function (el) {
-        el.closest(".c-message_kit__gutter").style.background = "#00800036";
-    });
+    if (document.querySelector(".p-view_header__channel_title.p-view_header__truncated_text").textContent == "tech-care"){
+        var l = document.querySelectorAll('[alt=":white_check_mark:"]');
+        l.forEach(function (el) {
+            el.closest(".c-message_kit__gutter").style.background = "#00800036";
+        });
+    }
+
 }
