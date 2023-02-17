fbc8c1faf7e61730f4435d8840ceb28167000c75
LGXerxes <ditisluca@yahoo.it>
2023-02-17 09:09:13 +0100

    For marta <3

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 setInterval(ch, 1200);
 
 function ch() {
-    if (document.querySelector(".p-view_header__channel_title.p-view_header__truncated_text").textContent == "tech-care"){
-        var l = document.querySelectorAll('[alt=":white_check_mark:"]');
+    if (document.querySelector(".p-view_header__channel_title.p-view_header__truncated_text").textContent == "tech-care") {
+        var l = document.querySelectorAll('[alt=":white_tick:"]');
         l.forEach(function (el) {
             el.closest(".c-message_kit__gutter").style.background = "#00800036";
         });
