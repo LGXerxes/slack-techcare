2942ff6459244f71e1c7247b96e80e4d7b968ca8
LGXerxes <ditisluca@yahoo.it>
2023-03-01 15:45:25 +0100

    added linking in #product

--- background.js
+++ background.js
@@ -1,11 +1,27 @@
 setInterval(ch, 1200);
 
 function ch() {
-    if (document.querySelector(".p-view_header__channel_title.p-view_header__truncated_text").textContent == "tech-care") {
+    channelname = document.querySelector(".p-view_header__channel_title.p-view_header__truncated_text")
+    if (!channelname) {
+        return
+    }
+    console.debug(channelname.textContent)
+    console.debug(channelname.textContent == "tech-care")
+
+    if (channelname.textContent == "tech-care") {
+        console.log("was tech-care")
         var l = document.querySelectorAll('[alt=":white_tick:"]');
         l.forEach(function (el) {
             el.closest(".c-message_kit__gutter").style.background = "#00800036";
         });
+    } else if (channelname.textContent == "product") {
+        l = document.querySelectorAll('.c-mrkdwn__code');
+        l.forEach(function (el) {
+            t = el.innerHTML
+            el.onclick = function () {
+                window.open(`https://linear.app/clerkio/issue/${t}`
+                )
+            };
+        });
     }
-
 }
